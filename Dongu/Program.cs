b67b763fca1b2662c75e23192b4c74eff6e24f1e
onurhanas <113357599+onurhanas@users.noreply.github.com>
2022-10-02 22:27:07 +0300

    Dongu

---
+++ Dongu/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Dongu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   Console.Write("Bir sayı giriniz: ");
+            int sayac =int.Parse(Console.ReadLine());
+             for (int i =1; i < sayac; i++)
+            {
+                if(i%2==1){
+                    Console.WriteLine(i);
+                }
+            }
+            int tekToplam =0;
+            int ciftToplam=0;
+            for (int i = 1; i <=1000; i++)
+            { if(i%2==1){
+                    tekToplam+=i;
+
+                }
+                else{
+                    ciftToplam+=i;
+                }
+                
+            }
+            Console.WriteLine(tekToplam);
+            Console.WriteLine(ciftToplam);
+        }
+    }
+}
